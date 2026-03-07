bbf36f1df98e64d88bc62d7021b08a4153d79d86
roya86 <royaabduallah@gmail.com>
2026-03-07 17:48:55 +0300

    1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,41 +1,25 @@
 #!/usr/bin/env python3
-"""Module contains function that returns pagination range
-Imports:
-    Tuple: Tuple type annotation
-    List: List type anotaton
-    csv: csv module
-"""
+"""Simple pagination module."""
 import csv
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Function returns pagination range
-
-    Args:
-        page (int): page number
-        page_size (int): page size
-
-    Returns:
-        Tuple[int, int]: start to end range
-    """
-    start = (page - 1) * page_size
-    end = page * page_size
-
-    return ((start, end))
+    """Return a tuple of size two containing start index and end index."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -45,17 +29,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Gets specific data
-        """
-        assert page > 0
-        assert page_size > 0
-        assert isinstance(page, int)
-        assert isinstance(page_size, int)
-        myRange = index_range(page, page_size)
-        start = myRange[0]
-        end = myRange[1]
-        filtered_list = self.dataset()
+        """Return the appropriate page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-        if start >= len(filtered_list):
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
             return []
-        return filtered_list[start:end]
+
+        return dataset[start_index:end_index]
