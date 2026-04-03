0321cd3a92e1891c1e7b831aa2caad841cad8ddd
roya86 <royaabduallah@gmail.com>
2026-04-03 10:13:37 +0000

    Task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return '';
+
+  const result = [];
+
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+
+  return result.join('-');
+Â}
